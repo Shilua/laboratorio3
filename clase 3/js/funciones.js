cc3512d9cb505d387b36e645f0e97f9aa315a5ed
shilua <shilua4@gmail.com>
2019-04-05 21:47:09 -0300

    ejemplocss y grillas

--- clase 3/js/funciones.js
+++ clase 3/js/funciones.js
@@ -1,12 +1,17 @@
 window.addEventListener("load",function() {
     var btnGuardar = document.getElementById('guardar');
+    var btnAgregar = document.getElementById('agregar');
+    var btnCerrar = document.getElementById('cerrar');
+    btnAgregar.addEventListener("click", aparecer);
     btnGuardar.addEventListener("click",guardar);
+    btnCerrar.addEventListener("click", cerrar);
 })
 
 function guardar(params) {
     var table = document.getElementById("tCuerpo");
     var nombre = document.getElementById('fname').value;
     var apellido = document.getElementById('lname').value;
+    
 
     if (apellido == "" || nombre =="") {
         document.getElementById("lname").className= "error";
@@ -19,4 +24,19 @@ function guardar(params) {
         document.getElementById("fname").className= "sinError";
         table.innerHTML += "<tr><td>"+nombre+"</td><td>"+apellido+"</td><td><a href =\"\">Borrar</a></td></tr>";
     }
+}
+
+function aparecer(params) {
+    var contAgregar = document.getElementById('contAgregar');
+    var btnAgregar = document.getElementById('agregar');
+    
+    contAgregar.hidden = false;
+    btnAgregar.hidden = true;
+}
+
+function cerrar(params) {
+    var contAgregar = document.getElementById('contAgregar');
+    var btnAgregar = document.getElementById('agregar');    
+    contAgregar.hidden = true;    
+    btnAgregar.hidden = false;
 }
