10fcdffd0087f41874d845e41b5904e6188d3620
shilua <shilua4@gmail.com>
2019-04-12 21:31:06 -0300

    adelanto de json

---
+++ clase 4/js/json.js
@@ -0,0 +1,14 @@
+var obj ={} //var esto es un json vacio
+var persona = {nombre:"maximiliano", edad:29, apellido: "vargas"} //esto es un json
+
+var array = [];
+var personas = [{nombre:"maximiliano", edad:29, apellido: "vargas"},
+{nombre:"martin", edad:29, apellido: "vargas"}]; //array de json
+
+var str = '{"nombre":"juan", "apellido": "perez"}';
+
+//convierte un string a json
+var json = JSON.parse(str);
+//convierte un json a string
+var str = JSON.stringify(json);
+
