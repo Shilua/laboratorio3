76d2021a46574d914336309b90e98a020d418dc1
shilua <shilua4@gmail.com>
2019-05-10 21:11:04 -0300

    primer parcial

---
+++ MaximilianoVargasPP3C/js/functionsIndex.js
@@ -0,0 +1,112 @@
+var request = new XMLHttpRequest();  
+window.onload = loadEvents;
+
+
+function loadEvents(){
+    get("buttonClose").addEventListener("click",cerrar);
+    loadTable();
+}
+
+function loadTable() {
+    request.open("GET", "http://localhost:3000/materias", true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.onreadystatechange = callback;
+    request.send();
+}
+
+function callback() {
+
+    if (request.readyState === 4) {
+        if (request.status === 200) {
+            var returnServer = JSON.parse(request.responseText);
+            var tbodyObject = get("tableBody");
+            returnServer.forEach(persona => {
+                var elementoTr = document.createElement('tr');
+                elementoTr.addEventListener('click', clickOnRow );
+                tbodyObject.appendChild(elementoTr);
+                elementoTr.appendChild(generateTd(persona.id));
+                elementoTr.appendChild(generateTd(persona.nombre));
+                elementoTr.appendChild(generateTd(persona.cuatrimestre));
+                elementoTr.appendChild(generateTd(persona.fechaFinal));
+                elementoTr.appendChild(generateTd(persona.turno)); 
+            });
+            localStorage.setItem('cachedData', JSON.stringify(returnServer));
+        }
+        else{
+            console.log("Error en la respuesta del servidor Numero: "+request.status);
+        }
+    }
+}
+
+function generateTd (data){
+
+    var elementoTd = document.createElement('td');
+    
+    var elementoText = document.createTextNode(data);
+    elementoTd.appendChild( elementoText );
+    return elementoTd;
+}
+
+function abrir(){
+    var $fieldSet = get("fieldset");
+    $fieldSet.hidden=false;
+
+}
+
+function cerrar(){
+    var $fieldSet = get("fieldset");
+    $fieldSet.hidden=true;
+
+}
+
+function get(id) {
+    var retorno = document.getElementById(id);
+    return retorno;
+}
+
+function deleteFunction(e){
+    e.preventDefault();
+    //console.log(e.target.parentNode.parentNode);
+    //console.log(e.srcElement.parentNode.parentNode);
+    var trToDelete = e.srcElement.parentNode.parentNode;
+    trToDelete.parentNode.removeChild( trToDelete );
+}
+
+function clickOnRow(e){
+    e.preventDefault();
+    var form = get('form');
+    abrir();
+    var id = e.target.parentNode.firstChild.firstChild.nodeValue;
+    var personas = JSON.parse(localStorage.getItem('cachedData'));
+    personas.forEach(persona => {
+        if(id == persona.id){
+
+            for (let i = 0; i < form.children.length; i++) {
+                const element = form.children[i];
+                    var input = element.getAttribute("name");
+
+                    
+
+                    if(input){
+                        console.log(input);
+                        if(input === "inputName"){
+                        form.children[i].setAttribute("value",persona.nombre );
+                        }
+                        else if (input === "cuatrimestre") {
+                            option = form.children[i].children;
+                            for (let i = 0; i < option.length; i++) {
+                                const element = option[i];
+                                var value = element.getAttribute("value");
+                                if(value == persona.cuatrimestre)
+                                {
+                                    console.log(value);
+                                }
+                                
+                            }
+                        }
+                    }
+            }
+        }
+
+    });
+}
